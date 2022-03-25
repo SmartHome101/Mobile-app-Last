0d63836073cace0f81bcd6fb1942c96996006cb6
Elalfy74 <mahmoudelalfy13@gmail.com>
2022-03-25 17:21:01 +0200

    Build fix

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -56,7 +56,7 @@ class _HomePageState extends State<HomePage> {
                   return LoginScreen();
                 }));
               },
-              child: Icon(
+              child: const Icon(
                 IconData(0xe3b3, fontFamily: 'MaterialIcons'),
                 color: Color(0xFF6F35A5),
               )),
