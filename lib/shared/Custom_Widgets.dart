8101233eafb97d9b825ebac20d5b1eef16a04a3c
Mohamed Nour <mohamednoursadek@gmail.com>
2022-07-14 12:13:54 +0200

    Update Custom_Widgets.dart

--- lib/shared/Custom_Widgets.dart
+++ lib/shared/Custom_Widgets.dart
@@ -9,105 +9,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 
-class On_Off_Widget extends StatefulWidget {
-  final item;
-  final update;
-  const On_Off_Widget(this.item, this.update);
 
-  @override
-  _On_Off_WidgetState createState() => _On_Off_WidgetState();
-}
-
-class _On_Off_WidgetState extends State<On_Off_Widget> {
-  late bool isActive;
-  late String key;
-
-  @override
-  Widget build(BuildContext context) {
-    var entryList = widget.item.entries.toList();
-    key = entryList[0].key;
-    isActive = entryList[0].value;
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: cardColor,
-        border: Border.all(
-          color: isActive ? (cardBorder_On_Color)! : cardBorder_Off_Color,
-          width: 3,
-        ),
-      ),
-      height: 150,
-      width: 150,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0).copyWith(bottom: 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image(
-                  image: AssetImage('icons/' + key + '.png'),
-                  height: 60,
-                  color: isActive ? Colors.deepPurple[200] : Colors.white,
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0)
-                .copyWith(left: 15, bottom: 0, top: 5, right: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  key,
-                  style: GoogleFonts.yantramanav(
-                    textStyle: TextStyle(
-                      fontSize: 20,
-                      color: isActive ? Colors.deepPurple[200] : Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                Text(
-                  isActive ? "On" : "Off",
-                  style: GoogleFonts.yantramanav(
-                    textStyle: TextStyle(
-                      fontSize: 20,
-                      color: isActive ? Colors.deepPurple[200] : Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                Switch(
-                  onChanged: (bool value) {
-                    setState(() {
-                      widget.update(key, value);
-                    });
-                  },
-                  activeColor: Colors.white,
-                  activeTrackColor: Colors.deepPurple[200],
-                  value: isActive,
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
 
 class Fire_Data extends StatefulWidget {
   final text;
@@ -416,6 +318,105 @@ ButtonStyle buttonStyle(Size _size) {
 }
 
 
+class On_Off_Widget extends StatefulWidget {
+  final item;
+  final update;
+  const On_Off_Widget(this.item, this.update);
+
+  @override
+  _On_Off_WidgetState createState() => _On_Off_WidgetState();
+}
+
+class _On_Off_WidgetState extends State<On_Off_Widget> {
+  late bool isActive;
+  late String key;
+
+  @override
+  Widget build(BuildContext context) {
+    var entryList = widget.item.entries.toList();
+    key = entryList[0].key;
+    isActive = entryList[0].value;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: cardColor,
+        border: Border.all(
+          color: isActive ? (cardBorder_On_Color)! : cardBorder_Off_Color,
+          width: 3,
+        ),
+      ),
+      height: 150,
+      width: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(bottom: 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage('icons/' + key + '.png'),
+                  height: 60,
+                  color: isActive ? Colors.deepPurple[200] : Colors.white,
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0)
+                .copyWith(left: 15, bottom: 0, top: 5, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  key,
+                  style: GoogleFonts.yantramanav(
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      color: isActive ? Colors.deepPurple[200] : Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: [
+                Text(
+                  isActive ? "On" : "Off",
+                  style: GoogleFonts.yantramanav(
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      color: isActive ? Colors.deepPurple[200] : Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Switch(
+                  onChanged: (bool value) {
+                    setState(() {
+                      widget.update(key, value);
+                    });
+                  },
+                  activeColor: Colors.white,
+                  activeTrackColor: Colors.deepPurple[200],
+                  value: isActive,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class CustomeButton extends StatelessWidget {
   CustomeButton({required this.title, this.onTap});
@@ -440,7 +441,6 @@ class CustomeButton extends StatelessWidget {
     );
   }
 }
-
 
 class CustomTextField extends StatelessWidget {
   CustomTextField({this.hintText, this.onChanged, this.isPassword, this.icon, required this.InputType});
@@ -481,7 +481,6 @@ class CustomTextField extends StatelessWidget {
     );
   }
 }
-
 
 enum inputType {UserName, HouseCode, Email, Password}
 
